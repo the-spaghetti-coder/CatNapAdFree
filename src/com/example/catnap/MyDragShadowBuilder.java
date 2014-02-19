6f6ccb6f893b36d5137ce8d5fdeed54d1bf645bf
unknown <jcedilla@clinical-jeff-PC.pathway.local>
2014-02-18 17:12:59 -0800

    Drag and drop.

--- src/com/example/catnap/MyDragShadowBuilder.java
+++ src/com/example/catnap/MyDragShadowBuilder.java
@@ -1,7 +1,35 @@
 package com.example.catnap;
 
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.view.View;
+import android.widget.ImageView;
+
+
 
 public class MyDragShadowBuilder extends View.DragShadowBuilder{
+	ImageView shadow;
 	
+		public MyDragShadowBuilder(View v) {
+			super(v);
+			shadow = (ImageView) v;
+			
+		}
+		
+		public void onProvideShadowMetrics (Point size, Point touch) {
+        // Defines local variables
+			int width, height;
+			width = getView().getWidth() * 2;
+			height = getView().getHeight() * 2;
+			
+			size.set(width, height);
+			touch.set(width / 2 , height / 2);
+		}	
+
+		public void onDrawShadow(Canvas canvas) {
+			shadow.draw(canvas);
+		}
+		
 }
+
--- src/com/example/catnap/NapActivity.java
+++ src/com/example/catnap/NapActivity.java
@@ -3,10 +3,13 @@ package com.example.catnap;
 import android.app.Activity;
 import android.content.ClipData;
 import android.os.Bundle;
+import android.view.DragEvent;
 import android.view.Menu;
 import android.view.View;
+import android.view.View.OnDragListener;
 import android.view.View.OnLongClickListener;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class NapActivity extends Activity {
 
@@ -15,16 +18,42 @@ public class NapActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_nap);
 		
-		ImageView sleepyCat = (ImageView)findViewById(R.id.sleepyCat);
+		final ImageView sleepyCat = (ImageView)findViewById(R.id.sleepyCat);
+		final ImageView droid = (ImageView)findViewById(R.id.testArea);
+		
 		sleepyCat.setTag("icon bitmap");
 		sleepyCat.setOnLongClickListener(new OnLongClickListener() {
 			
 			@Override
 			public boolean onLongClick(View v) {
 				System.out.println("Long click works!");
-//				ClipData.Item item = new ClipData.Item((CharSequence) v.getTag()); 
-//				ClipData dragData = new ClipData(v.getTag(),ClipData.MIMETYPE_TEXT_PLAIN,item);
-//				View.DragShadowBuilder myShadow = new MyDragShadowBuilder(sleepyCat);
+				String[] mimetypes = new String[]{"MIMETYPE_TEXT_PLAIN"};
+				ClipData.Item item = new ClipData.Item((CharSequence) v.getTag()); 
+				ClipData dragData = new ClipData((CharSequence) v.getTag(), mimetypes, item);
+				View.DragShadowBuilder myShadow = new MyDragShadowBuilder(sleepyCat);
+				Toast.makeText(NapActivity.this, "Drag started!", Toast.LENGTH_LONG).show();
+//				sleepyCat.setVisibility(4);  MAKES INVISIBLE
+				return v.startDrag(dragData, myShadow, null, 0);
+				
+			}
+		});
+		
+		droid.setOnDragListener(new OnDragListener() {
+			
+			@Override
+			public boolean onDrag(View arg0, DragEvent event) {
+				final int action = event.getAction();
+				switch(action) {
+				case DragEvent.ACTION_DRAG_ENDED:
+					Toast.makeText(NapActivity.this, "You dropped it! Woo!", Toast.LENGTH_SHORT).show();
+					break;
+				case DragEvent.ACTION_DRAG_ENTERED:
+					Toast.makeText(NapActivity.this, "Entered the bounding zone", Toast.LENGTH_SHORT).show();
+					break;
+				default:
+					System.out.println("fail");
+					break;
+				}
 				return false;
 			}
 		});
